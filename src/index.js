022498c460ee8955aa0d72382a4162fb79673c50
github NikolaCzv <github nikola.raicic@hotmail.com>
2019-11-20 11:58:23 -0600

    three challenges down

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 console.log('%c HI', 'color: firebrick')
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(resp => resp.json())
+.then(json => addImages(json))
+
+fetch('https://dog.ceo/api/breeds/list/all')
+.then(resp => resp.json())
+.then(json => addBreeds(json))
+
+let ul = document.getElementById("dog-breeds")
+
+
+ul.addEventListener('click', colorChange)
+
+function addImages(json) {
+    let div = document.querySelector("#dog-image-container")
+    json.message.forEach(image => {
+        let img = document.createElement('img')
+        img.src = `${image}`
+        div.append(img)
+    });
+}
+
+function addBreeds(json) {
+    Object.keys(json.message).forEach(breed => {
+        let li = document.createElement('li');
+        li.innerText = `${breed}`;
+        li.className = 'breedList'
+        ul.append(li);
+    })
+}
+
+function colorChange(event) {
+    if (event.target.className === 'breedList') {
+        if (event.target.style.color === 'red') {
+            event.target.style.color = 'black';
+        } else {
+            event.target.style.color = 'red';
+        }
+    }
+}
+
