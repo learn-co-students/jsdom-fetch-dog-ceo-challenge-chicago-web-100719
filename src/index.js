c2a6f3e9fb061da1ed940e9e423a4d2f48e3c194
NMoore42 <nicholas.moore5@icloud.com>
2019-11-20 14:34:31 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,52 +1,60 @@
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-const dogContainer = document.getElementById("dog-image-container");
-const dogBreedContainer = document.getElementById("dog-breeds");
-const breedUrl = 'https://dog.ceo/api/breeds/list/all';
-const dogFilter = document.getElementById("breed-dropdown");
-let dogBreeds = ""
+function main() {
 
-fetch(imgUrl)
-  .then(response => response.json())
-  .then(dogData => appendDogs(dogData.message))
-  .catch(err => console.log(err));
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  const dogContainer = document.getElementById("dog-image-container");
+  const dogBreedContainer = document.getElementById("dog-breeds");
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+  const dogFilter = document.getElementById("breed-dropdown");
+  let dogBreeds = "";
 
-fetch(breedUrl)
-  .then(response => response.json())
-  .then(dogData => dogBreeds = Object.keys(dogData.message))
-  .then(dogData => appendBreeds(dogBreeds))
-  .catch(err => console.log(err));
+  fetch(imgUrl)
+    .then(response => response.json())
+    .then(dogData => appendDogs(dogData.message))
+    .catch(err => console.log(err));
 
-function appendDogs(dogData) {
-  for (dogURL of dogData) {
-    const dogPic = document.createElement("img");
-    dogPic.src = dogURL;
-    dogPic.style = "width: 325px; height: 300px"
-    dogContainer.appendChild(dogPic);
+  fetch(breedUrl)
+    .then(response => response.json())
+    .then(dogData => dogBreeds = Object.keys(dogData.message))
+    .then(dogData => appendBreeds(dogBreeds))
+    .catch(err => console.log(err));
+
+  function appendDogs(dogData) {
+    for (dogURL of dogData) {
+      const dogPic = document.createElement("img");
+      dogPic.src = dogURL;
+      dogPic.style = "width: 325px; height: 300px"
+      dogContainer.appendChild(dogPic);
+    }
+  };
+
+  function appendBreeds(dogData) {
+    dogBreedContainer.innerHTML = ""
+    for (dogURL of dogData) {
+      const dogBreed = document.createElement("li");
+      dogBreed.textContent = dogURL
+      dogBreedContainer.appendChild(dogBreed);
+    }
+  };
+
+  function changeDogBreedColor() {
+    if (event.target.tagName === "LI") {
+      event.target.style.color = "blue";
+    }
   }
+
+  function filterDropDown() {
+    const startingLetter = event.target.value
+    const filteredDogsBreeds = dogBreeds.filter(breed => breed[0] === startingLetter)
+    appendBreeds(filteredDogsBreeds)
+  }
+
+
+  //EVENT LISTENERS
+  dogBreedContainer.addEventListener("click", changeDogBreedColor);
+  dogFilter.addEventListener("change", filterDropDown);
+
 };
 
-function appendBreeds(dogData) {
-  dogBreedContainer.innerHTML = ""
-  for (dogURL of dogData) {
-    const dogBreed = document.createElement("li");
-    dogBreed.textContent = dogURL
-    dogBreedContainer.appendChild(dogBreed);
-  }
-};
-
-function changeDogBreedColor() {
-  if (event.target.tagName === "LI") {
-    event.target.style.color = "blue";
-  }
-}
-
-function filterDropDown() {
-  const startingLetter = event.target.value
-  const filteredDogsBreeds = dogBreeds.filter(breed => breed[0] === startingLetter)
-  appendBreeds(filteredDogsBreeds)
-}
 
 
-//EVENT LISTENERS
-dogBreedContainer.addEventListener("click", changeDogBreedColor);
-dogFilter.addEventListener("change", filterDropDown);
+main();
