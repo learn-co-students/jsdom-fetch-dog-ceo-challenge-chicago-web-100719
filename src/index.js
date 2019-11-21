d9e413da9de4097d1e8d9fe0a6faf73f1a92e7f3
bosmanpa <bsmn.ptrck@gmail.com>
2019-11-21 15:58:42 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function(){
+    
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(json => json.message.forEach(element => {imageDisplay(element)}))
+
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(json => breedDisplay(json.message))
+
+const breed = document.querySelector('ul')
+const breedFilter = document.getElementById("breed-dropdown")
+const listItem = document.querySelectorAll("dog-breed")
+const filterOption = document.querySelector('li')
+
+breed.addEventListener("click", (e) => {
+    if (e.target.className === "dog-breed"){
+        e.target.style.color = "magenta"
+    }
+    })
+
+breedFilter.addEventListener("change", (e) => {
+    if (listItem.innerText !== e.target.value) {
+        listItem.style.visibility = "hidden"
+    }
+})    
+    
+})
+function breedDisplay(object){
+    for (const key in object){
+        let newLi = document.createElement('li')
+        newLi.innerText = key
+        newLi.className = "dog-breed"
+        document.getElementById("dog-breeds").appendChild(newLi)
+    }
+}
+
+function imageDisplay(imageURL){
+    let imageTag = document.createElement('img')
+    imageTag.src = imageURL
+    document.getElementById("dog-image-container").appendChild(imageTag)
+}
+
